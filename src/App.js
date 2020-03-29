c9a5b365d34a87c12d56e49307ef834a9e703b88
jatin77 <clockstopfountain@gmail.com>
2020-03-29 14:47:13 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+import { Route, Switch } from "react-router-dom";
+
+const Hat = () => <h1>Hats</h1>;
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={Hat} />
+      </Switch>
     </div>
   );
 }
